22d2c9a67ec9a3efcfa93b7c03fbe1cb04ead019
aruneskkuamr-codingx <aruneshkumar.pndt@gmail.com>
2026-01-03 21:16:09 +0530

    incomplete

---
+++ LEC.03-LEC/q3.cpp
@@ -0,0 +1,14 @@
+//Calculate the sum of odd numbers from 1 to n.
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cout << "ENTER THE VALUE OF N( number upto which u need the sum). Ans: ";
+    cin >> n;
+
+    int sum =0;
+
+    for(int i=1; i<=n; i++)
+    return 0;
+}
